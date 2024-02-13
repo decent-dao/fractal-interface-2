19fa4458a0a9928d1c72674e3abd9baffc6ffc9f
Adam Gall <galladamj@gmail.com>
2024-02-13 14:20:01 -0500

    Break QueryProvider into own component

--- src/Providers/Web3Provider.tsx
+++ src/Providers/Web3Provider.tsx
@@ -3,10 +3,6 @@ import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 
 import { WagmiProvider } from 'wagmi'
 import { mainnet, sepolia } from 'wagmi/chains'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// 0. Setup queryClient
-const queryClient = new QueryClient()
 
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = 'ef9ca770e347c286eded5e15e8154870'
@@ -41,7 +37,7 @@ createWeb3Modal({
 export default function Web3Provider({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      {children}
     </WagmiProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import Web3Provider from './Providers/Web3Provider.tsx'
 import UIProvider from './Providers/UIProvider.tsx'
+import QueryProvider from './Providers/QueryProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <UIProvider>
       <Web3Provider>
-        <App />
+        <QueryProvider>
+          <App />
+        </QueryProvider>
       </Web3Provider>
     </UIProvider>
   </React.StrictMode>,
