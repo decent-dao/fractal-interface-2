7ef1da8961f82c135e0bbbda0dc36c6d5b02ae98
Adam Gall <galladamj@gmail.com>
2024-02-13 13:49:24 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import ConnectButton from './ConnectButton'
+
+function App() {
+  return (
+    <>
+      <h1>Fractal</h1>
+      <div>
+        <ConnectButton />
+      </div>
+    </>
+  )
+}
+
+export default App
