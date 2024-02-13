bda86b617f0bae2b26aa0006bf71209d25d7dda9
Adam Gall <galladamj@gmail.com>
2024-02-13 16:23:45 -0500

    Make sepolia default chain

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -30,6 +30,7 @@ const config = defaultWagmiConfig({
 // 3. Create modal
 createWeb3Modal({
   wagmiConfig: config,
+  defaultChain: sepolia,
   projectId,
   themeMode: 'light',
   enableAnalytics: true // Optional - defaults to your Cloud configuration
