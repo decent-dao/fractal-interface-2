a6683a38b336d15d752cbba767de357f6d0bb27e
Adam Gall <galladamj@gmail.com>
2024-02-14 13:33:11 -0500

    Implement, as example, data fetching that doesn't block UI

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,51 @@
+import { Suspense } from "react"
+import { useLoaderData, useParams, defer, Await, useAsyncError } from "react-router-dom"
+
+function ErrorComponent() {
+  const error = useAsyncError() as string;
+
+  return (
+    <div>Error loading dashboard: {error}</div>
+  )
+}
+
 function Page() {
+  const params = useParams()
+  const data = useLoaderData() as { message: string }
+
   return (
     <div>
       <h1>DAO Dashboard</h1>
+      <pre>{params.id}</pre>
+      <Suspense
+        fallback={<div>Loading dashboard...</div>}
+      >
+        <Await
+          resolve={data.message}
+          errorElement={<ErrorComponent />}
+        >
+          {(message) => (
+            <div>{message}</div>
+          )}
+        </Await>
+      </Suspense>
     </div>
   )
 }
 
 function loader() {
-  return false
-}
+  const p = new Promise((resolve, reject) => {
+    return setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve("foobar")
+      } else {
+        reject("it just happens sometimes")
+      }
+    }, 2000)
+  })
 
+  return defer({ message: p })
+}
 
 export const dashboardRoute = {
   id: "dashboard",
@@ -17,4 +53,3 @@ export const dashboardRoute = {
   loader,
   element: <Page />,
 }
-
