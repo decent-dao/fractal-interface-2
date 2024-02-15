9f95253ea5575cd69c8926ac6f46e52ee026920d
Adam Gall <galladamj@gmail.com>
2024-02-15 09:31:38 -0500

    Show some network information on Landing

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,25 @@
-import { Flex, Button, Input } from '@chakra-ui/react'
+import { HStack, Button, Input, Box, VStack } from '@chakra-ui/react'
+import { useBlockNumber, useChainId, useChains } from 'wagmi'
 
 function Landing () {
+  const chainId = useChainId()
+  const chains = useChains()
+  const block = useBlockNumber({ watch: true })
+
+  const currentChainName = chains.find(chain => chain.id === chainId)?.name
+
   return (
-    <Flex>
-      <Input placeholder='Enter a Safe address' />
-      <Button ml={2}>Load</Button>
-    </Flex>
+    <VStack align='stretch'>
+      <HStack>
+        <Input placeholder='Enter a Safe address' />
+        <Button>Load</Button>
+      </HStack>
+      <Box>
+        <Box>All chains: {chains.map(c => c.name).join(', ').toString()}</Box>
+        {currentChainName != null && <Box>Current Chain: {currentChainName} ({chainId})</Box>}
+        {block.data != null && <Box>Current Block: {block.data.toString()}</Box>}
+      </Box>
+    </VStack>
   )
 }
 
