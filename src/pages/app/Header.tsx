2c60878a7fde838f228ea0f6499e4fc02a8a9d56
Adam Gall <galladamj@gmail.com>
2024-02-15 13:22:56 -0500

    Add text to Header displaying which network (config) Wagmi is using

--- src/pages/app/Header.tsx
+++ src/pages/app/Header.tsx
@@ -1,7 +1,12 @@
-import { Container, Flex, HStack, Heading, Spacer } from "@chakra-ui/react";
+import { Box, Container, Flex, HStack, Heading, Spacer } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useChains, useChainId } from "wagmi";
 
 function Header() {
+  const chainId = useChainId();
+  const chains = useChains();
+  const currentChainName = chains.find((chain) => chain.id === chainId)?.name;
+
   return (
     <Container my={2}>
       <Flex>
@@ -13,6 +18,9 @@ function Header() {
           <w3m-button />
           <w3m-network-button />
         </HStack>
+      </Flex>
+      <Flex justifyContent={"end"}>
+        <Box fontSize="xs">Wagmi connected to {currentChainName}</Box>
       </Flex>
     </Container>
   );
