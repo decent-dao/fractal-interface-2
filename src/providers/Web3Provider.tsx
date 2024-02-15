dd4d098438e8ea563eceaf9efd83f5db19c1f7d3
Adam Gall <galladamj@gmail.com>
2024-02-15 13:29:41 -0500

    Remove custom transport, update WalletConnect metadata

--- src/providers/Web3Provider.tsx
+++ src/providers/Web3Provider.tsx
@@ -1,7 +1,7 @@
 import { createWeb3Modal } from "@web3modal/wagmi/react";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
 
-import { WagmiProvider, http } from "wagmi";
+import { WagmiProvider } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 
 // 1. Get projectId at https://cloud.walletconnect.com
@@ -9,9 +9,9 @@ const projectId = "ef9ca770e347c286eded5e15e8154870";
 
 // 2. Create wagmiConfig
 const metadata = {
-  name: "Web3Modal",
-  description: "Web3Modal Example",
-  url: "https://web3modal.com", // origin must match your domain & subdomain
+  name: "Fractal",
+  description: "Do more with DAOs",
+  url: "https://app.fractalframework.xyz", // origin must match your domain & subdomain
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
 };
 
@@ -25,10 +25,6 @@ const config = defaultWagmiConfig({
   enableEIP6963: true, // Optional - true by default
   enableCoinbase: true, // Optional - true by default
   // ...wagmiOptions // Optional - Override createConfig parameters
-  transports: {
-    [mainnet.id]: http("https://rpc.ankr.com/eth"),
-    [sepolia.id]: http("https://rpc.ankr.com/eth_sepolia"),
-  },
 });
 
 // 3. Create modal
