30d9a63f5742d76c6d5473855c293403e9505e2c
Adam Gall <galladamj@gmail.com>
2024-02-19 17:00:11 -0500

    Add typing to env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_DESCRIPTION: string;
+  readonly VITE_APP_URL: string;
+  readonly VITE_APP_ICON: string;
+
+  readonly VITE_WALLETCONNECT_METADATA_NAME: string;
+  readonly VITE_WALLETCONNECT_METADATA_DESCRIPTION: string;
+  readonly VITE_WALLETCONNECT_METADATA_URL: string;
+  readonly VITE_WALLETCONNECT_METADATA_ICON: string;
+
+  readonly VITE_WALLETCONNECT_PROJECT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
