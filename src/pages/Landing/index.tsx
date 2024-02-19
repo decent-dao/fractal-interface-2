daadd1a8b2b47516ac5fcc892a6869efdf4a89e7
Adam Gall <galladamj@gmail.com>
2024-02-19 09:46:44 -0500

    Make "Load" button navigate to Home page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -10,12 +10,15 @@ import {
 import { useChainId } from "wagmi";
 import { networkIdToPrefix } from "../../utils/network";
 import Network from "./Network";
+import { useNavigate } from "react-router-dom";
 
 function Landing() {
   const chainId = useChainId();
   const networkPrefix = networkIdToPrefix(chainId);
 
   const [address, setAddress] = useState("");
+
+  const navigate = useNavigate();
 
   return (
     <VStack align="start">
@@ -30,7 +33,13 @@ function Landing() {
             }}
           />
         </InputGroup>
-        <Button>Load</Button>
+        <Button
+          onClick={() => {
+            navigate(`home?safe=${networkPrefix}:${address}`);
+          }}
+        >
+          Load
+        </Button>
       </HStack>
       <Network />
     </VStack>
